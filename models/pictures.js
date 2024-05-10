ebf838cdf2244e559a3940369028f211d01f7404
CaptainBeatty <juliengrange.dev@gmail.com>
2024-05-10 11:48:52 +0200

    some tries3

--- models/pictures.js
+++ models/pictures.js
@@ -1,17 +1,19 @@
 
 const pictures = 
 [
-	{ id: "1", url: "http://localhost:8000/images/image-1.jpg", title: "beach", description: "C'était une belle balade. Il y avait le soleil...et Ksenia.",device: "Iphone SE1"},
-	{ id: "2", url: "http://localhost:8000/images/image-2.jpg", title: "boat", description: "C'était une belle balade. Il y avait le soleil...et Svetlana.",device: "Iphone SE1"},
-	{ id: "3", url: "http://localhost:8000/images/image-3.jpg", title: "forest", description: "C'était une belle balade. Il y avait le soleil...et Anastasia." ,device: "Iphone SE1"},
-	{ id: "4", url: "http://localhost:8000/images/image-4.jpg", title: "city", description: "C'était une belle balade. Il y avait le soleil...et Alexandra." ,device: "Iphone SE1"},
-	{ id: "5", url: "http://localhost:8000/images/image-5.jpg", title: "italy", description: "C'était une belle balade. Il y avait le soleil...et Katerina." ,device: "Iphone SE1"},
-	{ id: "6", url: "http://localhost:8000/images/jardinerie.jpg", title: "jardinerie", description: "Un paradie en ville." ,device: "Iphone SE1"},
-	{ id: "7", url: "http://localhost:8000/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
-	{ id: "8", url: "http://localhost:8000/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
-	{ id: "9", url: "http://localhost:8000/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
-	{ id: "10", url: "http://localhost:8000/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
-	{ id: "11", url: "http://localhost:8000/images/thoissey.jpg", title: "Thoissey", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
+	{ id: "1", url: "https://picturescope-api.onrender.com/images/image-1.jpg", title: "beach", description: "Une belle photo",device: "Iphone SE1"},
+	{ id: "2", url: "https://picturescope-api.onrender.com/images/image-2.jpg", title: "boat", description: "Une belle photo",device: "Iphone SE1"},
+	{ id: "3", url: "https://picturescope-api.onrender.com/images/image-3.jpg", title: "forest", description: "Une belle photo" ,device: "Iphone SE1"},
+	{ id: "4", url: "https://picturescope-api.onrender.com/images/image-4.jpg", title: "city", description: "Une belle photo" ,device: "Iphone SE1"},
+	{ id: "5", url: "https://picturescope-api.onrender.com/images/image-5.jpg", title: "italy", description: "Une belle photo" ,device: "Iphone SE1"},
+	{ id: "6", url: "https://picturescope-api.onrender.com/images/jardinerie.jpg", title: "jardinerie", description: "Un paradie en ville." ,device: "Iphone SE1"},
+	{ id: "7", url: "https://picturescope-api.onrender.com/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
+	{ id: "8", url: "https://picturescope-api.onrender.com/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
+	{ id: "9", url: "https://picturescope-api.onrender.com/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
+	{ id: "10", url: "https://picturescope-api.onrender.com/images/thoissey.jpg", title: "little bridge", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
+	{ id: "11", url: "https://picturescope-api.onrender.com/images/thoissey.jpg", title: "Thoissey", description: "Une balade hivernale avant Noël",device: "Iphone SE1"},
+
+	
 	
   ]
 
