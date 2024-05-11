e5498a4b689185504346821529ac46c208b08683
CaptainBeatty <juliengrange.dev@gmail.com>
2024-05-11 18:20:29 +0200

    test3

--- routes/pictures.js
+++ routes/pictures.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const picturesCtrl = require('../controllers/pictures')
 
-
-router.get('/:id', picturesCtrl.getOnePicture);
 router.get('/', picturesCtrl.getAllPicture);
+router.get('/:id', picturesCtrl.getOnePicture);
+
 
 
 module.exports = router
