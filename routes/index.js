5df67261e5c34391b99ac3f00ff1d3e44982ece3
CaptainBeatty <juliengrange.dev@gmail.com>
2024-05-14 23:55:06 +0200

    begin post and some middleware

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router()
 const getPictures = require('../controllers/pictures')
 const getPicture = require('../controllers/picture')
 
-
+router.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	next();
+  });
 
 router.get('/picture', function (req, res) {
 	const { id } = req.query
@@ -19,6 +24,13 @@ router.get('/picture', function (req, res) {
 router.get('/pictures', function (req, res, next) {
 	const picturesList = getPictures()
 	res.send({ picturesList })
+})
+
+router.post('/pictures', function (req, res, next) {
+	console.log(req.body);
+	res.status(201).json({
+	  message: 'Image ajoutée !'
+	});
 })
 
 
