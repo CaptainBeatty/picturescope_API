6e93ca205d28fb506c765d9c0117aa503e017279
CaptainBeatty <juliengrange.dev@gmail.com>
2024-06-08 16:47:51 +0200

    middelware upgrade

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-    // try {
-    //     const token = req.headers.authorization.split(" ")[1];
-    //     const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
-    //     const userId = decodedToken.userId;
-    //     req.auth = {
-    //         userId : userId
-    //     };
-    //     if (req.body.userId && req.body.userId !== userId) {
-    //         throw "Invalid user ID";
-    //     } else {
-    //         next();
-    //     }
-    // } catch {
-    //     res.status(403).json({
-    //         error: new Error("403: unauthorized request.")
-    //     });
-    // }
 
-// Middleware d'authentification
-
+  if (req.method === 'GET') {
+    // Autoriser les requêtes GET sans token
+    return next();
+  }
+   
     try {
       const token = req.headers.authorization.split(' ')[1];
       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
